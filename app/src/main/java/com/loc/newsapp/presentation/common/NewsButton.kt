286936d050921250e562c8903869c48e8feda121
Naveen Kushwaha <naveenk121972@gmail.com>
2024-06-02 14:52:48 +0530

    First commit

--- app/src/main/java/com/loc/newsapp/presentation/common/NewsButton.kt
+++ app/src/main/java/com/loc/newsapp/presentation/common/NewsButton.kt
@@ -5,10 +5,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -25,6 +25,20 @@ fun NewsButton(
         shape = RoundedCornerShape(size = 6.dp)
     ){
         Text(text , style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold))
+    }
+}
+
+@Composable
+fun NewsTextButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    TextButton(onClick = onClick){
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            color = Color.White
+        )
     }
 }
 
